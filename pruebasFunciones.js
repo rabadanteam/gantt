cc6afeb99e84b9c9aa301ae4afb3694dc98f58af
Marco A. Grimaldo <40187581+MarcoGrimado@users.noreply.github.com>
2019-04-11 11:05:23 -0500

    Separacion vergas

---
+++ pruebasFunciones.js
@@ -0,0 +1,15 @@
+function crearTarea()
+{
+	let fecha1 = new Date(2019, 2, 1);
+	let fecha2 = new Date(2019, 2, 11);
+	let fecha3 = new Date(2019, 2, 21);
+
+	let tareaPadre = new Tarea(0, fecha1, fecha3, "Tarea Padre", 0, null);
+	let tareaHija1 = new Tarea(1, fecha1, fecha2, "Hija 1", 100, null);
+	let tareaHija2 = new Tarea(2, fecha1, fecha3, "Hija 2", 50, null);
+
+	tareaPadre.addHijo(tareaHija1);
+	tareaPadre.addHijo(tareaHija2);
+
+	alert(tareaPadre.calcularAvance());
+}
